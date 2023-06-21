26f7ce3c65a107e2047655cf9a8420d51dd5e4c7
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2023-06-21 16:08:36 +0900

    06/21 게시판 상세페이지 작성

--- Django/myapp/blog/urls.py
+++ Django/myapp/blog/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     #path( 패턴, 매핑 ) /blog/
-    path("",views.Index.as_view(), name='list'),
+    
     #글 조회
-    path("write/",views.Index.write, name='write'),
+    path("",views.List.as_view(), name='list'),
+    #글 상세조회
+    path("detail/<int:pk>",views.Detail.as_view(),name='detail'), #blog/detail/1
     #글 작성
+    path("write/",views.Write.as_view(), name='write'), #blog/write
     #글 수정
     #글 삭제
     #코멘트 작성
--- Django/myapp/blog/views.py
+++ Django/myapp/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.views import View
+from django.views.generic import ListView, CreateView, DeleteView
 from .models import Post
 from .forms import PostForm
-
+from django.urls import reverse_lazy
 # Create your views here.
 # 어떤 응답을 해 줄건지
 
@@ -40,3 +40,24 @@ class Index(View):
         #아닐때는 새롭게 form을 생성해준다
         form = PostForm()
         return render(request, 'blog/write.html',{'form' : form} )
+    
+
+# Django 자체의 클래스 뷰 기능도 강력하고 편리
+# django.views.generic -> ListView
+class List(ListView):
+    model = Post #모델이 어떤 건지 보여주고
+    template_name='blog/post_list.html'
+    context_object_name = 'posts'
+    
+
+
+class Write(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:list')
+
+
+class Detail(DeleteView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
